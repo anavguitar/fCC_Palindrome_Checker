eb9a40999bd8d9900f569be38bfdd7fabfa1b2ce
anavguitar <anavguitar@gmail.com>
2024-04-04 15:39:14 -0700

    consolidated HTML element structures, added JS const vars and the palindromeChecker(), added some more CSS styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+const textInput = document.getElementById('text-input');
+const checkBtn = document.getElementById('check-btn');
 
 //this function checks the string if its a palindrome
 function palindromeChecker(str) {
@@ -10,17 +11,21 @@ function palindromeChecker(str) {
     if(word.length > 0) {
         if (word === joinRPW) {
             console.log(`${word} is a palindrome`);
-            newH2.innerText = `${word} is a palindrome`;
+            result.innerText = `${word} is a palindrome`;
         }
         else {
             console.log(`${word} is not a palindrome!`);
-            newH2.innerText = `${word} is not a palindrome!`
+            result.innerText = `${word} is not a palindrome!`
         }
     } else {
         console.log(`Please input a value`);
-        newH2.innerText = `Please input a value`;
+        result.innerText = `Please input a value`;
     }
-
-    
 }
-
+let liveWrd = '';
+checkBtn.addEventListener('click', e => {
+    liveWrd = textInput.value;
+    e.preventDefault();
+    palindromeChecker(liveWrd);
+    console.log(liveWrd);
+})
