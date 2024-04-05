7b31e754d62ba1142f75d84e2145d303ed533946
anavguitar <anavguitar@gmail.com>
2024-04-04 20:59:31 -0700

    added a regex function and changed some of the core logic behind palindromeChecker()

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 
+
+function cleanInputString(str) {
+    const regex = /[^a-z^A-Z]+/g;
+    return str.replace(regex, '');
+  }
+
 //this function checks the string if its a palindrome
 function palindromeChecker(str) {
-    let word = str;
-    let splitWord = word.split('');
-    let reverseSplWrd = splitWord.reverse();
-    let joinRPW = reverseSplWrd.join('');
+    let word = str;//this is the original string
+    let cleanWord = cleanInputString(str);//this cleans out any non alphabetical chars
+    let lowerCaseWord = cleanWord.toLowerCase();//this makes the phrase lowercase
+    let splitWord = lowerCaseWord.split('');//this splits all the letters into an indexed array
+    let reverseSplWrd = splitWord.reverse();//this reverses the array
+    let joinRPW = reverseSplWrd.join('');//this joins the array into a string
 
     if(word.length > 0) {
-        if (word === joinRPW) {
+        if (lowerCaseWord === joinRPW) {
             console.log(`${word} is a palindrome`);
             result.innerText = `${word} is a palindrome`;
         }
@@ -22,7 +30,7 @@ function palindromeChecker(str) {
         result.innerText = `Please input a value`;
     }
 }
-let liveWrd = '';
+// let liveWrd = '';
 checkBtn.addEventListener('click', e => {
     liveWrd = textInput.value;
     e.preventDefault();
